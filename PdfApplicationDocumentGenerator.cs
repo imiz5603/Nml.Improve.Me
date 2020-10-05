32d8a83da4ba55d0fcde676f5e20e140cb7aa62a
imran <imran@DESKTOP-UCJLKF1>
2020-10-05 20:33:58 +0200

    Commit 2 : -Corrected access modifiers. -Added Single responsibility methods - Corrected inconsistent naming of fields

--- PdfApplicationDocumentGenerator.cs
+++ PdfApplicationDocumentGenerator.cs
@@ -6,12 +6,13 @@ namespace Nml.Improve.Me
 {
     public class PdfApplicationDocumentGenerator : IApplicationDocumentGenerator
     {
-        private readonly IDataContext DataContext;
-        private IPathProvider _templatePathProvider;
-        public IViewGenerator View_Generator;
-        internal readonly IConfiguration _configuration;
-        private readonly ILogger<PdfApplicationDocumentGenerator> _logger;
-        private readonly IPdfGenerator _pdfGenerator;
+        //Access modifiers and naming convention corrected. These fields only used here
+        private readonly IDataContext _dataContext;
+        private static IPathProvider _templatePathProvider;
+        private static IViewGenerator _viewGenerator;
+        private static IConfiguration _configuration;
+        private static ILogger<PdfApplicationDocumentGenerator> _logger;
+        private static IPdfGenerator _pdfGenerator;
 
         public PdfApplicationDocumentGenerator(
             IDataContext dataContext,
@@ -24,28 +25,27 @@ namespace Nml.Improve.Me
             if (dataContext != null)
                 throw new ArgumentNullException(nameof(dataContext));
 
-            DataContext = dataContext;
+            _dataContext = dataContext ?? throw new ArgumentNullException("dataContext");
             _templatePathProvider = templatePathProvider ?? throw new ArgumentNullException("templatePathProvider");
-            View_Generator = viewGenerator;
+            _viewGenerator = viewGenerator ?? throw new ArgumentNullException("viewGenerator");
             _configuration = configuration ?? throw new ArgumentNullException("configuration");
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _pdfGenerator = pdfGenerator;
+            _pdfGenerator = pdfGenerator ?? throw new ArgumentNullException(nameof(pdfGenerator));
         }
 
         public byte[] Generate(Guid applicationId, string baseUri)
         {
             //Use SingleOrDefault to cater for null return
-            Application application = DataContext.Applications.SingleOrDefault(app => app.Id == applicationId);
+            Application application = _dataContext.Applications.SingleOrDefault(app => app.Id == applicationId);
 
             if (application != null)
             {
-                //Substring will result in incorrect url with 1 character
+                //Substring will result in incorrect baseUri with 1 character
                 //Ensure last slash is removed only
                 if (baseUri.EndsWith("/"))
                     baseUri = baseUri.Substring(0, baseUri.Length - 1);
 
-                string view;
-                string path;
+                var view = "";
 
                 //Do NULL check on PERSON object and throw exception
                 if (application.Person == null)
@@ -54,78 +54,13 @@ namespace Nml.Improve.Me
                 switch (application.State)
                 {
                     case ApplicationState.Pending:
-                        path = _templatePathProvider.Get("PendingApplication");
-                        PendingApplicationViewModel PendingApplicationViewModel = new PendingApplicationViewModel
-                        {
-                            ReferenceNumber = application.ReferenceNumber,
-                            State = application.State.ToDescription(),
-                            //changed for consistency
-                            FullName = string.Format("{0} {1}", application.Person.FirstName, application.Person.Surname),
-                            AppliedOn = application.Date,
-                            SupportEmail = _configuration.SupportEmail,
-                            Signature = _configuration.Signature
-                        };
-                        view = View_Generator.GenerateFromPath(string.Format("{0}{1}", baseUri, path), PendingApplicationViewModel);
+                        view = GetPendingApplicationView(application, baseUri);
                         break;
                     case ApplicationState.Activated:
-                        path = _templatePathProvider.Get("ActivatedApplication");
-                        ActivatedApplicationViewModel ActivatedApplicationViewModel = new ActivatedApplicationViewModel
-                        {
-                            ReferenceNumber = application.ReferenceNumber,
-                            State = application.State.ToDescription(),
-                            //changed for consistency
-                            FullName = string.Format("{0} {1}", application.Person.FirstName, application.Person.Surname),
-                            LegalEntity = application.IsLegalEntity ? application.LegalEntity : null,
-                            //Assume that Legal entity can have a value evn when IsLegalEntity = 0
-                            PortfolioFunds = application.Products.SelectMany(p => p.Funds),
-                            PortfolioTotalAmount = application.Products.SelectMany(p => p.Funds)
-                                                            //Below calculation results in calculating tax ONLY, not amount + tax(Assuming TaxRate is a percentage i.e 0.15)
-                                                            //.Select(f => (f.Amount - f.Fees) * _configuration.TaxRate)
-                                                            .Select(f => (f.Amount - f.Fees) + ((f.Amount - f.Fees) * _configuration.TaxRate))
-                                                            .Sum(),
-                            AppliedOn = application.Date,
-                            SupportEmail = _configuration.SupportEmail,
-                            Signature = _configuration.Signature
-                        };
-                        view = View_Generator.GenerateFromPath(string.Format("{0}{1}", baseUri, path), ActivatedApplicationViewModel);
+                        view = GetActivatedApplicationView(application, baseUri);
                         break;
                     case ApplicationState.InReview:
-                        path = _templatePathProvider.Get("InReviewApplication");
-                        //strongly typed for readability purposes
-                        string inReviewMessage = "Your application has been placed in review" +
-                                            application.CurrentReview.Reason switch
-                                            {
-                                                { } reason when reason.Contains("address") =>
-                                                    " pending outstanding address verification for FICA purposes.",
-                                                { } reason when reason.Contains("bank") =>
-                                                    " pending outstanding bank account verification.",
-                                                _ =>
-                                                    " because of suspicious account behaviour. Please contact support ASAP."
-                                            };
-                        //make below model into a block like the others
-                        InReviewApplicationViewModel inReviewApplicationViewModel = new InReviewApplicationViewModel
-                        {
-                            ReferenceNumber = application.ReferenceNumber,
-                            State = application.State.ToDescription(),
-                            FullName = string.Format("{0} {1}", application.Person.FirstName, application.Person.Surname),
-                            LegalEntity = application.IsLegalEntity ? application.LegalEntity : null,
-                            PortfolioFunds = application.Products.SelectMany(p => p.Funds),
-                            PortfolioTotalAmount =
-                                                   application.Products.SelectMany(p => p.Funds)
-                                                    //Below calculation results in calculating tax ONLY, not amount + tax(Assuming TaxRate is a percentage i.e 0.15)
-                                                    //.Select(f => (f.Amount - f.Fees) * _configuration.TaxRate)
-                                                    .Select(f => (f.Amount - f.Fees) + ((f.Amount - f.Fees) * _configuration.TaxRate))
-                                                   .Sum(),
-                            InReviewMessage = inReviewMessage,
-                            InReviewInformation = application.CurrentReview,
-                            AppliedOn = application.Date,
-                            SupportEmail = _configuration.SupportEmail,
-                            Signature = _configuration.Signature,
-
-                        };
-
-                        //Changed for consistency
-                        view = View_Generator.GenerateFromPath(string.Format("{0}{1}", baseUri, path), inReviewApplicationViewModel);
+                        view = GetInReviewApplicationView(application, baseUri);
                         break;
                     default:
                         _logger.LogWarning(
@@ -156,5 +91,90 @@ namespace Nml.Improve.Me
                 //throw error instead of return null
             }
         }
+        private static string GetPendingApplicationView(Application application, string baseUri)
+        {
+            
+            var path = _templatePathProvider.Get("PendingApplication");
+
+            PendingApplicationViewModel PendingApplicationViewModel = new PendingApplicationViewModel
+            {
+                ReferenceNumber = application.ReferenceNumber,
+                State = application.State.ToDescription(),
+                //changed for consistency
+                FullName = string.Format("{0} {1}", application.Person.FirstName, application.Person.Surname),
+                AppliedOn = application.Date,
+                SupportEmail = _configuration.SupportEmail,
+                Signature = _configuration.Signature
+            };
+            return _viewGenerator.GenerateFromPath(string.Format("{0}{1}", baseUri, path), PendingApplicationViewModel);
+
+        }
+        private static string GetActivatedApplicationView(Application application, string baseUri)
+        {
+
+            var path = _templatePathProvider.Get("ActivatedApplication");
+            ActivatedApplicationViewModel ActivatedApplicationViewModel = new ActivatedApplicationViewModel
+            {
+                ReferenceNumber = application.ReferenceNumber,
+                State = application.State.ToDescription(),
+                //changed for consistency
+                FullName = string.Format("{0} {1}", application.Person.FirstName, application.Person.Surname),
+                LegalEntity = application.IsLegalEntity ? application.LegalEntity : null,
+                //Assume that Legal entity can have a value evn when IsLegalEntity = 0
+                PortfolioFunds = application.Products.SelectMany(p => p.Funds),
+                PortfolioTotalAmount = application.Products.SelectMany(p => p.Funds)
+                                                //Below calculation results in calculating tax ONLY, not amount + tax(Assuming TaxRate is a percentage i.e 0.15)
+                                                //.Select(f => (f.Amount - f.Fees) * _configuration.TaxRate)
+                                                .Select(f => (f.Amount - f.Fees) + ((f.Amount - f.Fees) * _configuration.TaxRate))
+                                                .Sum(),
+                AppliedOn = application.Date,
+                SupportEmail = _configuration.SupportEmail,
+                Signature = _configuration.Signature
+            };
+            return _viewGenerator.GenerateFromPath(string.Format("{0}{1}", baseUri, path), ActivatedApplicationViewModel);
+
+        }
+        private static string GetInReviewApplicationView(Application application, string baseUri)
+        {
+            var path = _templatePathProvider.Get("InReviewApplication");
+            //strongly typed for readability purposes
+            string inReviewMessage = "Your application has been placed in review" +
+                                application.CurrentReview.Reason switch
+                                {
+                                    { } reason when reason.Contains("address") =>
+                                        " pending outstanding address verification for FICA purposes.",
+                                    { } reason when reason.Contains("bank") =>
+                                        " pending outstanding bank account verification.",
+                                    _ =>
+                                        " because of suspicious account behaviour. Please contact support ASAP."
+                                };
+            //make below model into a block like the others
+            InReviewApplicationViewModel inReviewApplicationViewModel = new InReviewApplicationViewModel
+            {
+                ReferenceNumber = application.ReferenceNumber,
+                State = application.State.ToDescription(),
+                FullName = string.Format("{0} {1}", application.Person.FirstName, application.Person.Surname),
+                LegalEntity = application.IsLegalEntity ? application.LegalEntity : null,
+                PortfolioFunds = application.Products.SelectMany(p => p.Funds),
+                PortfolioTotalAmount =
+                                       application.Products.SelectMany(p => p.Funds)
+                                        //Below calculation results in calculating tax ONLY, not amount + tax(Assuming TaxRate is a percentage i.e 0.15)
+                                        //.Select(f => (f.Amount - f.Fees) * _configuration.TaxRate)
+                                        .Select(f => (f.Amount - f.Fees) + ((f.Amount - f.Fees) * _configuration.TaxRate))
+                                       .Sum(),
+                InReviewMessage = inReviewMessage,
+                InReviewInformation = application.CurrentReview,
+                AppliedOn = application.Date,
+                SupportEmail = _configuration.SupportEmail,
+                Signature = _configuration.Signature,
+
+            };
+
+            //Changed for consistency
+            return _viewGenerator.GenerateFromPath(string.Format("{0}{1}", baseUri, path), inReviewApplicationViewModel);
+
+        }
     }
+
+
 }
